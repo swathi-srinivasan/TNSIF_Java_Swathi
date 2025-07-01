aee0882317ef5c3244f057b075e0a75e502b48eb
user <user@H-Sys43>
2025-07-01 11:36:29 +0530

    this is my oops concept of java

---
+++ java_programs/src/oops_concepts/Executor.java
@@ -0,0 +1,14 @@
+package oops_concepts;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		EncapsulationDemo ed = new EncapsulationDemo();
+		ed.setName("MS Dhoni");
+		ed.setAge(43);
+		ed.setJersyNo(7);
+		System.out.println(ed);
+		
+	}
+
+}
